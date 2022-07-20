2da817d5bba8f39104fa0236b18906086c8fb5a4
최진합 <pppooo95@naver.com>
2022-07-20 11:01:23 +0900

    feat - 버튼 컴포넌트 / S07P12C104-9

--- client/src/pages/DummyPage/DummyPage.jsx
+++ client/src/pages/DummyPage/DummyPage.jsx
@@ -1,12 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import API from '../../api';
 import styled from 'styled-components';
 import colors from '../../constants/colors';
 import TopNavigation from '../../components/Common/TopNavigation/TopNavigation';
+import Button from '../../components/Common/Button/Button';
 
 const DummyPage = () => {
+	const [input, setInput] = useState('');
+	const [btnState, setBtnState] = useState(false);
+	const check = () => {
+		if (input === '윤주혜') {
+			return true;
+		}
+		return false;
+	};
+	useEffect(() => {
+		setBtnState(check());
+	}, [input]);
 	useEffect(() => {
 		const fetch = async () => {
 			const res = await API.get('/test');
@@ -18,7 +30,10 @@ const DummyPage = () => {
 		<>
 			<TopNavigation
 				backClick
-				centerContent={<span>Fitwell</span>}
+				onBackClick={() => {
+					alert('클릭');
+				}}
+				leftContent={<span>Fitwell</span>}
 				rightMenu={
 					<>
 						<button>asd</button>
@@ -27,10 +42,19 @@ const DummyPage = () => {
 					</>
 				}
 			/>
+			<input type="text" value={input} onChange={e => setInput(e.target.value)} />
 			<div style={{ height: '400px' }}>옷 공유 합니다 1234</div>
-			<div style={{ height: '400px' }}>testseet</div>
-			<div style={{ height: '400px' }}>옷 공유 합니다</div>
-			<div style={{ height: '400px' }}>옷 공유 합니다</div>
+			<Button type="normal" label="다음 단계" style={{ marginBottom: 10 }} />
+			<Button type="kakao" label="다음 단계" style={{ marginBottom: 10 }} />
+			<Button type="active" label="다음 단계" style={{ marginBottom: 10 }} />
+			<Button
+				type={btnState ? 'active' : 'disabled'}
+				label="다음 단계"
+				style={{ marginBottom: 10 }}
+				onClick={() => {
+					alert('클릭');
+				}}
+			/>
 		</>
 	);
 };
