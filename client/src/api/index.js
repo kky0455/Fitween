7465b0728bac5732dc691c09892dfabd5616537f
spiderq2697 <spiderq10@gmail.com>
2022-07-28 12:07:33 +0900

    feat: 테스트를 위해 API의 baseURL을 MSW 주소로 변경한다

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-	baseURL: process.env.REACT_APP_API_URI,
+	baseURL: process.env.REACT_APP_MOCK_API_URI,
+	withCredentials: true,
 });
 
 export default API;
