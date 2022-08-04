5148543613c219197231c1cfd170e87c09d80c34
david <abovenormal5023@gmail.com>
2022-08-04 16:44:59 +0900

    feat: 회원가입 기능 구현

--- backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -1,118 +1,59 @@
 package com.ssafy.api.controller;
 
-import com.google.api.client.http.javanet.NetHttpTransport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ssafy.api.request.UserLoginPostReq;
-import com.ssafy.api.response.UserLoginPostRes;
 import com.ssafy.api.service.UserService;
-import com.ssafy.common.model.response.BaseResponseBody;
-import com.ssafy.common.util.JwtTokenUtil;
-import com.ssafy.db.entity.User;
-import com.ssafy.db.repository.UserRepositorySupport;
-
-import io.swagger.annotations.Api;
+import com.ssafy.db.dto.Message;
+import com.ssafy.db.dto.StatusEnum;
+import com.ssafy.db.dto.UserSaveRequestDto;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import java.nio.charset.Charset;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
 
 /**
  * 인증 관련 API 요청 처리를 위한 컨트롤러 정의.
  */
 @Api(value = "인증 API", tags = {"Auth."})
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
-	@Autowired
-	UserService userService;
-	
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	
-	@PostMapping("/login")
-	@ApiOperation(value = "로그인", notes = "<strong>아이디와 패스워드</strong>를 통해 로그인 한다.") 
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공", response = UserLoginPostRes.class),
-        @ApiResponse(code = 401, message = "인증 실패", response = BaseResponseBody.class),
-        @ApiResponse(code = 404, message = "사용자 없음", response = BaseResponseBody.class),
-        @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
-    })
-	public ResponseEntity<UserLoginPostRes> login(@RequestBody @ApiParam(value="로그인 정보", required = true) UserLoginPostReq loginInfo) throws GeneralSecurityException, IOException {
-//		String userId = loginInfo.getId();
-//		String password = loginInfo.getPassword();
-		String loginType = loginInfo.getLoginType();
-		String token = loginInfo.getToken();
 
-		System.out.println(loginType);
+    private final UserService userService;
 
-		// from google
+    @ApiOperation(value = "회원가입",notes = "email과 password를 받아서 회원가입을 한다.")
+    @PostMapping("/login")
+    public ResponseEntity<?> signUp(@RequestBody UserSaveRequestDto requestDto){
+        Message message = new Message();
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        try {
+            String userid = userService.join(requestDto.toEntity());
 
-//		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
-		GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+            message.setStatus(StatusEnum.OK);
+            message.setMessage("회원가입 성공");
+            return new ResponseEntity<>(message, headers, HttpStatus.OK);
+        } catch (IllegalStateException e){
+            e.printStackTrace();
+            message.setStatus(StatusEnum.BAD_REQUEST);
+            message.setMessage("가입 이메일이 중복됩니다.");
+            return new ResponseEntity<String>("OVERLAP", HttpStatus.BAD_REQUEST);
+        } catch (Exception e){
+            e.printStackTrace();
+            message.setStatus(StatusEnum.INTERNAL_SERVER_ERROR);
+            message.setMessage("서버 에러 발생");
+            return new ResponseEntity<>(message, headers,  HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-				// Specify the CLIENT_ID of the app that accesses the backend:
-				.setAudience(Collections.singletonList(CLIENT_ID))	//oauth 클라이언트 id
-						// Or, if multiple clients access the backend:
-						//.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
-						.build();
+    }
 
 
-		// (Receive idTokenString by HTTPS POST)
 
-		GoogleIdToken idToken = verifier.verify(token);
-		System.out.println(token);
 
-		if (idToken != null) {
-			Payload payload = idToken.getPayload();
-
-			// Print user identifier
-			String userId = payload.getSubject();
-			System.out.println("User ID: " + userId);
-
-			// Get profile information from payload
-			String email = payload.getEmail();
-			boolean emailVerified = Boolean.valueOf(payload.getEmailVerified());
-			String name = (String) payload.get("name");
-			String pictureUrl = (String) payload.get("picture");
-			String locale = (String) payload.get("locale");
-			String familyName = (String) payload.get("family_name");
-			String givenName = (String) payload.get("given_name");
-
-			// Use or store profile information
-			// ...
-
-			System.out.println(email);
-		} else {
-			System.out.println("Invalid ID token.");
-		}
-
-		//
-		
-		//User user = userService.getUserByUserId(userId);
-		// 로그인 요청한 유저로부터 입력된 패스워드 와 디비에 저장된 유저의 암호화된 패스워드가 같은지 확인.(유효한 패스워드인지 여부 확인)
-		//if(passwordEncoder.matches(password, user.getPassword())) {
-			// 유효한 패스워드가 맞는 경우, 로그인 성공으로 응답.(액세스 토큰을 포함하여 응답값 전달)
-			//return ResponseEntity.ok(UserLoginPostRes.of(200, "Success", JwtTokenUtil.getToken(userId)));
-		//}
-		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
-		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
-	}
 }
