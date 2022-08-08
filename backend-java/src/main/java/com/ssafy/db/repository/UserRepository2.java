a037c8ee59ed403e6eade9cae3a343e3cc1fa5dd
david <abovenormal5023@gmail.com>
2022-08-08 10:09:57 +0900

    refactor: 필요없는 기능 수정

--- backend-java/src/main/java/com/ssafy/db/repository/UserRepository2.java
+++ backend-java/src/main/java/com/ssafy/db/repository/UserRepository2.java
@@ -13,8 +13,5 @@ import java.util.Optional;
 public interface UserRepository2 extends JpaRepository<User, Long> {
 
 
-    boolean deleteByUserId(User user);
-    // 아래와 같이, Query Method 인터페이스(반환값, 메소드명, 인자) 정의를 하면 자동으로 Query Method 구현됨.
-    Optional<User> findByUserId(String userId);
-    boolean findByUserIdEquals(String userId);
+    Optional<User> findByEmail(String email);
 }
