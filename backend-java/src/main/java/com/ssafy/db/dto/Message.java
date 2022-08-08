ad24a2a4481fc9ae4c1fc31f1977dbbf1e4eab23
david <abovenormal5023@gmail.com>
2022-08-08 10:04:27 +0900

    refactor: 프로젝트에 맞게 message 수정

--- backend-java/src/main/java/com/ssafy/db/dto/Message.java
+++ backend-java/src/main/java/com/ssafy/db/dto/Message.java
@@ -6,12 +6,13 @@ import lombok.Data;
 public class Message {
 
     private StatusEnum status;
-    private String message;
-    private Object data;
+    private String responseType;
+    private String userId;
+    private Object accessToken, refreshToken;
 
     public Message(){
         this.status = StatusEnum.BAD_REQUEST;
-        this.data = null;
-        this.message = null;
+        this.userId = null;
+        this.responseType = null;
     }
 }
