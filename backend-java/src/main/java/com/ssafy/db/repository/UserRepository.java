8aaf482caa9216b64bf25b83d72a3951f44d9264
david <abovenormal5023@gmail.com>
2022-08-08 10:09:41 +0900

    refactor: userId로 검사 feat: username 으로 회원검사

--- backend-java/src/main/java/com/ssafy/db/repository/UserRepository.java
+++ backend-java/src/main/java/com/ssafy/db/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.ssafy.db.repository;
 
+import com.querydsl.core.types.EntityPath;
+import com.querydsl.jpa.JPAExpressions;
+import com.ssafy.db.entity.QUser;
 import com.ssafy.db.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -16,15 +19,17 @@ public class UserRepository {
 
     @PersistenceContext
     private final EntityManager em;
+    private JPAExpressions jpaQueryFactory;
+    QUser qUser = QUser.user;
 
     public void save(User user){
         user.setRoles(Collections.singletonList("ROLE_USER")); // 최초 가입 시 USER로 설정
         em.persist(user);
     }
 
-    public boolean existsByEmail(String email){
-        List<User> memberList = em.createQuery("select m from User m where m.email = :email", User.class)
-                .setParameter("email", email)
+    public boolean existsByUserId(String userId){
+        List<User> memberList = em.createQuery("select m from User m where m.userId = :userId", User.class)
+                .setParameter("userId", userId)
                 .getResultList();
         if(memberList.size() == 0) return false;
         return true;
@@ -36,6 +41,13 @@ public class UserRepository {
                 .getResultList();
         if(memberList.size() == 0) throw new IllegalStateException("해당 이메일을 가진 사용자가 없습니다.");
         return memberList.get(0);
+    }
+
+    public User findUserByName(String name) {
+        User user = jpaQueryFactory.select(qUser).from(qUser)
+                .where(qUser.name.eq(name)).fetchOne();
+        if(user == null) return null;
+        return user;
     }
 
 
