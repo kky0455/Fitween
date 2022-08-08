231a9607e08dc0e42d6e75bd65add6987bc05385
david <abovenormal5023@gmail.com>
2022-08-08 10:08:07 +0900

    feat: 회원가입시 필요한 바디 정의

--- backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
+++ backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
@@ -1,5 +1,6 @@
 package com.ssafy.api.request;
 
+import com.ssafy.db.entity.User;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -16,6 +17,8 @@ public class UserRegisterPostReq {
 	String id;
 	@ApiModelProperty(name="유저 Password", example="your_password")
 	String password;
+	@ApiModelProperty(name="유저 고유번호", example = "15612315684")
+	String userId;
 	@ApiModelProperty(name="유저 이름", example = "한세환")
 	String name;
 	@ApiModelProperty(name="유저 프사", example = "유저 프사 링크")
@@ -29,11 +32,26 @@ public class UserRegisterPostReq {
 	@ApiModelProperty(name="유저 닉네임", example = "씽코")
 	String nickname;
 	@ApiModelProperty(name="유저 키", example = "182")
-	int height;
+	double height;
 	@ApiModelProperty(name="유저 몸무게", example = "78")
-	int weight;
+	double weight;
 	@ApiModelProperty(name="유저 신발사이즈", example = "280")
 	int footSize;
 	@ApiModelProperty(name="유저 사는 지역", example = "광주 서구 치평동")
 	String region;
+
+	public User toEntity(){
+		User user = new User();
+		user.setUserId(userId);
+		user.setName(name);
+		user.setEmail(email);
+		user.setAge(age);
+		user.setGender(gender);
+		user.setNickname(nickname);
+		user.setHeight(height);
+		user.setWeight(weight);
+		user.setFootSize(footSize);
+		user.setRegion(region);
+		return user;
+	}
 }
