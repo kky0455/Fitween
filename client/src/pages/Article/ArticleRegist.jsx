f83bccbb8e8679954ada013f1f21499d5ab037d3
최진합 <pppooo95@naver.com>
2022-08-09 15:20:49 +0900

    feat - 피드등록에서 사진등록 , 캐러셀로 미리보기 구현

--- client/src/pages/Article/ArticleRegist.jsx
+++ client/src/pages/Article/ArticleRegist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
@@ -7,22 +7,36 @@ import colors from '../../constants/colors';
 import TopNavigation from '../../components/Common/TopNavigation/TopNavigation';
 import Button from '../../components/Common/Button/Button';
 import Input from '../../components/Common/Input/Input';
-import modify_img from '../../assets/modify_img.png';
 import { registArticle } from '../../api/article';
 import TextArea from '../../components/Common/TextArea/TextArea';
+import Carousel from '../../components/Common/Carousel/Carousel';
 
 const ArticleRegist = () => {
 	const [title, setTitle] = useState('');
 	const [price, setPrice] = useState('');
 	const [content, setContent] = useState('');
+	const [imageFiles, setImageFiles] = useState(null);
+	const [imageSrcs, setImageSrcs] = useState([]);
+
+	const fileRef = useRef(null);
 	const navigate = useNavigate();
+	const encodeFileToBase64 = fileBlob => {
+		const reader = new FileReader();
+		reader.readAsDataURL(fileBlob);
+		return new Promise(resolve => {
+			reader.onload = () => {
+				setImageSrcs(prev => [...prev, reader.result]);
+				resolve();
+			};
+		});
+	};
 	const onSubmitHandler = async () => {
-		const body = {
-			title: title,
-			price: price,
-			content: content,
-		};
-		const ret = await registArticle(body);
+		const formData = new FormData();
+		formData.append('title', title);
+		formData.append('price', price);
+		formData.append('content', content);
+		formData.append('photo', imageFiles);
+		const ret = await registArticle(formData);
 		if (ret.result === 'success') navigate(`/article/${ret.articleId}`);
 	};
 	return (
@@ -41,16 +55,33 @@ const ArticleRegist = () => {
 				}}
 			>
 				{/* 사진 등록 */}
-				<img
-					src={modify_img}
-					alt=""
-					css={css`
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						width: 100%;
-					`}
-				/>
+				<form name="imageForm">
+					<Carousel
+						style={{ marginBottom: '30px' }}
+						contentHeight="300px"
+						imgSrcList={imageSrcs}
+						onClick={e => {
+							fileRef.current.click();
+						}}
+					/>
+					<input
+						ref={fileRef}
+						name="file"
+						type="file"
+						multiple
+						accept="image/*"
+						style={{ display: 'none' }}
+						onChange={e => {
+							if (e.target.files.length <= 5) {
+								setImageSrcs([]);
+								Array.from(e.target.files).forEach(file => encodeFileToBase64(file));
+								setImageFiles(e.target.files);
+							} else {
+								alert('최대 5개만 업로드할 수 있습니다.');
+							}
+						}}
+					/>
+				</form>
 				<div
 					css={css`
 						display: flex;
