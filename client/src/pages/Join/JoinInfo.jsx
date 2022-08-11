3e3ec040bab6033b810bab0c18da3879a8314694
spiderq2697 <spiderq10@gmail.com>
2022-08-11 17:33:32 +0900

    feat: 추가정보 입력 시 유효성을 검사한다
    - 닉네임, 키, 몸무게, 발사이즈에 대한 유효성을 검사한다

--- client/src/pages/Join/JoinInfo.jsx
+++ client/src/pages/Join/JoinInfo.jsx
@@ -4,30 +4,35 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Common/Button/Button';
 import Input from '../../components/Common/Input/Input';
 import TopNavigation from '../../components/Common/TopNavigation/TopNavigation';
+import {
+	validateFootSize,
+	validateHeight,
+	validateNickName,
+	validateWeight,
+	validateAllInput,
+} from '../../utils/validate';
 
 const JoinInfo = ({ info, onChangeHandler }) => {
 	const [readyState, setReadyState] = useState(false);
+	const [nickNameErrMsg, setNickNameErrMsg] = useState('');
 	const navigate = useNavigate();
-	// toDo : 별명 중복검사 필요
-	const check = info => {
-		return info.nickname &&
-			info.nickname.trim().length > 0 &&
-			info.height &&
-			info.height.trim().length > 0 &&
-			info.weight &&
-			info.weight.trim().length > 0 &&
-			info.footSize &&
-			info.footSize.trim().length > 0
-			? true
-			: false;
-	};
+
 	useEffect(() => {
-		if (check(info)) {
+		const { state, type } = validateNickName(info.nickname);
+
+		if (validateAllInput(info)) {
 			setReadyState(true);
 		} else {
 			setReadyState(false);
 		}
+
+		if (!state) {
+			if (type === 'notLengthErr')
+				setNickNameErrMsg('별명은 특수문자, 이모지, 숫자를 입력받을 수 없습니다.');
+			else if (type === 'lengthErr') setNickNameErrMsg('별명은 2자 이상 8자 이하로 입력해주세요.');
+		}
 	}, [info]);
+
 	return (
 		<>
 			<TopNavigation
@@ -50,7 +55,8 @@ const JoinInfo = ({ info, onChangeHandler }) => {
 				</h2>
 				<Input
 					placeholder="별명"
-					errMsg="중복된 별명입니다"
+					error={info.nickname.length === 0 || validateNickName(info.nickname).state ? false : true}
+					errMsg={nickNameErrMsg}
 					type="text"
 					onChange={onChangeHandler}
 					value={info.nickname}
@@ -58,6 +64,8 @@ const JoinInfo = ({ info, onChangeHandler }) => {
 				/>
 				<Input
 					placeholder="키"
+					error={info.height.length === 0 || validateHeight(info.height) ? false : true}
+					errMsg="키는 90cm 이상 250cm 이하로 입력해주세요."
 					unit="cm"
 					type="number"
 					name="height"
@@ -66,6 +74,8 @@ const JoinInfo = ({ info, onChangeHandler }) => {
 				/>
 				<Input
 					placeholder="몸무게"
+					error={info.weight.length === 0 || validateWeight(info.weight) ? false : true}
+					errMsg="몸무게는 30kg 이상 200kg 이하로 입력해주세요."
 					unit="kg"
 					type="number"
 					name="weight"
@@ -74,6 +84,8 @@ const JoinInfo = ({ info, onChangeHandler }) => {
 				/>
 				<Input
 					placeholder="발사이즈"
+					error={info.footSize.length === 0 || validateFootSize(info.footSize) ? false : true}
+					errMsg="발사이즈는 0mm 이상 350mm 이하로 입력해주세요."
 					unit="mm"
 					type="number"
 					name="footSize"
