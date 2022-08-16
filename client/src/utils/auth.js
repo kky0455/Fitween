fdca0e5450545e8a47e2187755ecb45c33022900
spiderq2697 <spiderq10@gmail.com>
2022-08-16 19:01:53 +0900

    feat: 새로고침 또는 refreshToken 유효기간이 만료됐을 때 토큰을 재발급한다

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,23 +1,24 @@
 import { JWT_EXPIRY_TIME } from '../constants/config';
-import { setRefreshToken } from '../storage/Cookie';
+import { getRefreshToken, setRefreshToken } from '../storage/Cookie';
 import API from '../api';
 import * as authApi from '../api/auth';
 
 export const onRefresh = async () => {
-	const refreshInLocalStorage = {
-		refreshToken: localStorage.getItem('refreshToken'),
+	const refreshToken = {
+		refreshToken: getRefreshToken(),
 	};
-	const { refreshToken, accessToken } = await authApi.refresh(
-		JSON.stringify(refreshInLocalStorage),
-	);
 
-	onLogin(refreshToken, accessToken);
+	try {
+		const { refreshToken, accessToken } = await authApi.refresh(refreshToken);
+		onLoginSuccess(refreshToken, accessToken);
+	} catch (err) {
+		console.log(err);
+	}
 };
 
-export const onLogin = (refreshToken, accessToken) => {
+export const onLoginSuccess = (refreshToken, accessToken) => {
 	setRefreshToken(refreshToken);
-	localStorage.setItem('refreshToken', refreshToken);
 	API.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 
-	setTimeout(onRefresh(), JWT_EXPIRY_TIME - 60 * 1000);
+	setTimeout(onRefresh, JWT_EXPIRY_TIME - 60 * 1000);
 };
