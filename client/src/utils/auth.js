dcbbfba9165a20397733b2f26a7377f71318f762
spiderq2697 <spiderq10@gmail.com>
2022-08-17 10:22:12 +0900

    feat: 로그아웃 기능을 구현한다

---
+++ client/src/utils/auth.js
@@ -0,0 +1,29 @@
+import { JWT_EXPIRY_TIME } from '../constants/config';
+import { getRefreshToken, removeCookieToken, setRefreshToken } from '../storage/Cookie';
+import API from '../api';
+import * as authApi from '../api/auth';
+
+export const onRefresh = async () => {
+	const refreshToken = {
+		refreshToken: getRefreshToken(),
+	};
+
+	try {
+		const { refreshToken, accessToken } = await authApi.refresh(refreshToken);
+		onLoginSuccess(refreshToken, accessToken);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export const onLoginSuccess = (refreshToken, accessToken) => {
+	setRefreshToken(refreshToken);
+	API.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+	setTimeout(onRefresh, JWT_EXPIRY_TIME - 60 * 1000);
+};
+
+export const onLogout = () => {
+	removeCookieToken();
+	API.defaults.headers.common['Authorization'] = '';
+};
